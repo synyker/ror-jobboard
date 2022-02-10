c216cf7d7b97e901c18e776f95fab45945a53c5c
Eetu Mattila <eetu_mattila@hotmail.com>
2022-02-10 10:54:12 +0200

    Filter out old jobs from requests
    As quick patch only. Due to front-end limitations makes it also impossible to list expired jobs.

--- backend/app/controllers/jobs_controller.rb
+++ backend/app/controllers/jobs_controller.rb
@@ -7,7 +7,7 @@ class JobsController < ApplicationController
   # GET /jobs
   # GET /jobs.json
   def index
-    @jobs = Job.all
+    @jobs = Job.where("end >= ?", Date.today)
   end
 
   # GET /jobs/1
